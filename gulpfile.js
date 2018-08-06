7a669b3cbeb1704a772c0feb0772c2d5dec80f5c
Mateusz Kryszczak <mkrysz1337@gmail.com>
2018-08-06 19:17:41 +0200

    Started migration to separate repo

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+﻿const gulp = require('gulp');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+let devMode = false;
+
+const styles = [
+    "./src/css/**/*.css",
+    "./node_modules/bootstrap/dist/css/bootstrap.css"
+];
+
+const scripts = [
+    "./node_modules/angular/angular.js",
+    "./node_modules/angular-route/angular-route.js",
+    "./node_modules/jquery/dist/jquery.js",
+    "./node_modules/bootstrap/dist/js/bootstrap.js",
+    "./src/js/**/*.js"
+];
+
+gulp.task('css', function () {
+    gulp.src(styles)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('js', function () {
+    gulp.src(scripts)
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('html', function () {
+    gulp.src('./src/templates/**/*.html')
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('build', function () {
+    gulp.start(['css', 'js', 'html']);
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init(null, {
+        open: false,
+        server: {
+            baseDir: 'dist'
+        }
+    });
+});
+
+gulp.task('start', function () {
+    devMode = true;
+    gulp.start(['build', 'browser-sync']);
+    gulp.watch(['./src/css/**/*.css'], ['css']);
+    gulp.watch(['./src/js/**/*.js'], ['js']);
+    gulp.watch(['./src/templates/**/*.html'], ['html']);
+})
---
+++ src/js/app.js
@@ -0,0 +1,49 @@
+﻿var app = angular.module("smartHomeApp", ['ngRoute']);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "charts.html",
+            controller: "ChartsController",
+            auth: true
+        })
+        .when("/charts", {
+            templateUrl: "charts.html",
+            controller: "ChartsController",
+            auth: true
+        })
+        .when("/control", {
+            templateUrl: "control.html",
+            controller: "NodesController",
+            auth: true
+        })
+        .when("/nodes", {
+            templateUrl: "nodes.html",
+            controller: "NodesController",
+            auth: true
+        })
+        .when("/users", {
+            templateUrl: "users.html",
+            controller: "UsersController",
+            auth: true
+        })
+        .when("/my-profile", {
+            templateUrl: "user.html",
+            controller: "UserController",
+            auth: true
+        })
+        .when("/login", {
+            templateUrl: "login.html",
+            controller: "LoginController"
+        })
+        .otherwise({
+            templateUrl: "not-found.html"
+        });
+});
+
+app.controller("RootController", function ($scope) {
+
+    $scope.test = 'TEST';
+
+});
