09dec48a0b2adfe152614564c9f86dd019a1aaab
Mateusz Kryszczak <mkrysz1337@gmail.com>
2018-08-06 21:46:24 +0200

    proceeding with migration

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,22 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 var url = require('url');
 const browserSync = require('browser-sync').create();
-const httpProxy = require('http-proxy');
 const proxy = require('proxy-middleware');
 
 let devMode = false;
 
 const stylesDev = [
-    "./src/css/**/*.css",
     "./node_modules/bootstrap/dist/css/bootstrap.css",
-    "./node_modules/js-datepicker/datepicker.css"
+    "./node_modules/js-datepicker/datepicker.css",
+    "./node_modules/@fortawesome/fontawesome-free/css/all.css",
+    "./src/css/**/*.css"
 ];
 
 const stylesProd = [
-    "./src/css/**/*.css",
     "./node_modules/bootstrap/dist/css/bootstrap.min.css",
-    "./node_modules/js-datepicker/datepicker.min.css"
+    "./node_modules/js-datepicker/datepicker.min.css",
+    "./node_modules/@fortawesome/fontawesome-free/css/all.min.css",
+    "./src/css/**/*.css"
 ];
 
 const scriptsDev = [
@@ -40,6 +41,15 @@ const scriptsProd = [
     "./node_modules/chart.js/dist/Chart.min.js",
     "./src/js/**/*.js"
 ];
+
+const fonts1 = [
+    "./node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff2",
+    "./node_modules/bootstrap/fonts/glyphicons-halflings-regular.woff"
+]
+
+const webFonts1 = [
+    escape("./node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2")
+]
 
 gulp.task('css', function () {
     gulp.src(devMode ? stylesDev : stylesProd)
@@ -86,22 +96,40 @@ gulp.task('html', function () {
         }));
 });
 
+gulp.task('fonts', function () {
+    gulp.src(fonts1)
+        .pipe(gulp.dest('./dist/fonts/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('webFonts', function () {
+    gulp.src(webFonts1)
+        .pipe(gulp.dest('./dist/webfonts/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
 gulp.task('build', function () {
-    gulp.start(['css', 'js', 'html']);
+    gulp.start(['css', 'js', 'html', 'fonts', 'webFonts']);
 });
 
 
-
 gulp.task('browser-sync', function () {
-    var proxyOptions = url.parse('http://localhost/api/');
-    proxyOptions.route = '/api/';
+    let proxyOptionsApi = url.parse('http://localhost/api/');
+    proxyOptionsApi.route = '/api/';
+
+    let proxyOptionsImg = url.parse('http://localhost/img/');
+    proxyOptionsImg.route = '/img/';
 
     browserSync.init(null, {
         open: false,
         port: 3000,
         server: {
             baseDir: 'dist',
-            middleware: [proxy(proxyOptions)]
+            middleware: [proxy(proxyOptionsApi), proxy(proxyOptionsImg)]
         }
     });
 });
